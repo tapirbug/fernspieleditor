78d7d93cdeeb432b69899fe679772cec226b07dd
krachzack <hello@phstadler.com>
2019-10-04 22:34:57 +0200

    chore: add parcel dependency and upgrade
    Add `parcel` dependency and upgrade all dependencies. Also inline the scripts into the `package.json` to make sure the versions from the package are used.

--- scripts/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-URL="https://krachzack.github.io/fernspieleditor"
-mkdir -p docs && rm -rf docs && mkdir -p docs && \
-parcel build index.html \
-  --out-dir docs \
-  --no-source-maps \
-  --public-url $URL
-
--- scripts/dev.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-parcel index.html
