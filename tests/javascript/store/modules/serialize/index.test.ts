d727b76461efe79d71ea03384a2662d40528c6e4
krachzack <hello@phstadler.com>
2020-05-12 19:53:42 +0200

    style: fix lints in serialize/index.test.ts

--- tests/javascript/store/modules/serialize/index.test.ts
+++ tests/javascript/store/modules/serialize/index.test.ts
@@ -1,43 +1,23 @@
 import {
-  SerializeActions
+  SerializeActions,
+  serializeActionMapping
 } from '../../../../../assets/javascript/store/modules/serialize/serialize-actions'
 import Vue from 'vue'
-import Vuex, { Store } from 'vuex'
+import Vuex, { Store, mapActions } from 'vuex'
 import statesModule from '../../../../../assets/javascript/store/modules/states/states-module'
 import infoModule from '../../../../../assets/javascript/store/modules/info/info-module'
 import transitionsModule from '../../../../../assets/javascript/store/modules/transitions/index'
 import serializeModule from '../../../../../assets/javascript/store/modules/serialize/serialize-module'
 import soundsModule from '../../../../../assets/javascript/store/modules/sounds/sounds-module'
 import undoModule from '../../../../../assets/javascript/store/modules/undo/undo-module'
-import {
-  ADD_STATE,
-  REMOVE_STATE,
-  CONTINUE_UPDATE_STATE,
-  UPDATE_STATE,
-  UNDO,
-  REDO,
-  FOCUS_STATE,
-  SET_INITIAL_STATE,
-  ADD_TRANSITION,
-  REMOVE_TRANSITION,
-  SERIALIZE,
-  TO_YAML,
-  ADD_SOUND,
-  REMOVE_SOUND,
-  UPDATE_SOUND,
-  SERIALIZE_SOUNDS
-} from '../../../../../assets/javascript/store/action-types'
 import { TransitionGetters } from '../../../../../assets/javascript/store/modules/transitions/transitions-getters'
 import { StatesGetters } from '../../../../../assets/javascript/store/modules/states/states-getters'
-import { StatesActions } from 'assets/javascript/store/modules/states/states-actions'
-import { StateSpec, StateSummary } from 'assets/javascript/store/modules/states/state'
-import { UndoActions } from 'assets/javascript/store/modules/undo/undo-actions'
-import { FernspieleditorExtVersion } from 'assets/javascript/phonebook/phonebook-fernspieleditor-ext'
-import { TransitionsActions } from 'assets/javascript/store/modules/transitions/transitions-actions'
-import { TransitionSpec, TransitionType } from 'assets/javascript/store/modules/transitions/transition'
-import { SoundsActions } from 'assets/javascript/store/modules/sounds/sounds-actions'
-import { SoundSpec } from 'assets/javascript/store/modules/sounds/sound-spec'
-import { PhonebookSubsetForSounds } from 'assets/javascript/store/modules/sounds/sounds-phonebook-subset'
+import { StatesActions, statesActionMapping } from '../../../../../assets/javascript/store/modules/states/states-actions'
+import { UndoActions, undoActionMapping } from '../../../../../assets/javascript/store/modules/undo/undo-actions'
+import { FernspieleditorExtVersion } from '../../../../../assets/javascript/phonebook/phonebook-fernspieleditor-ext'
+import { TransitionsActions, transitionActionMapping } from '../../../../../assets/javascript/store/modules/transitions/transitions-actions'
+import { TransitionType } from '../../../../../assets/javascript/store/modules/transitions/transition'
+import { SoundsActions, soundsActionMapping } from '../../../../../assets/javascript/store/modules/sounds/sounds-actions'
 
 Vue.use(Vuex)
 
@@ -46,15 +26,15 @@ describe('serialize module', () => {
     expect.assertions(3)
     const { actions } = initTestContext()
     const sound = await actions.addSound({ id: 'SoundA', name: 'Sound A', volume: 0.68 })
-    actions.addState({ id: 'FromState', name: 'FromState', sounds: [sound.id] })
-    actions.addState({ id: 'ToState', name: 'ToState' })
-    actions.addTransition({
+    await actions.addState({ id: 'FromState', name: 'FromState', sounds: [sound.id] })
+    await actions.addState({ id: 'ToState', name: 'ToState' })
+    await actions.addTransition({
       type: TransitionType.Timeout,
       after: 5,
       from: 'FromState',
       to: 'ToState'
     })
-    actions.setInitialState('FromState')
+    await actions.setInitialState('FromState')
     const { states, transitions } = await actions.serialize()
     expect(Object.keys(states)).toEqual(
       expect.arrayContaining(['FromState', 'ToState'])
@@ -118,36 +98,36 @@ vendor:
 `
     const { actions } = initTestContext()
     const sound = await actions.addSound({ id: 'SoundA', name: 'Sound A', volume: 0.68 })
-    actions.addState({ id: 'FromState', name: 'FromState', sounds: [sound.id] })
-    actions.updateState({
+    await actions.addState({ id: 'FromState', name: 'FromState', sounds: [sound.id] })
+    await actions.updateState({
       id: 'FromState',
       position: {
         x: 501,
         y: 602
       }
     })
-    actions.addState({ id: 'ToState', name: 'ToState' })
-    actions.addTransition({
+    await actions.addState({ id: 'ToState', name: 'ToState' })
+    await actions.addTransition({
       type: TransitionType.Timeout,
       after: 5,
       from: 'FromState',
       to: 'ToState'
     })
-    actions.setInitialState('FromState')
-    actions.focusState('ToState')
+    await actions.setInitialState('FromState')
+    await actions.focusState('ToState')
     const yaml = await actions.toYaml()
     expect(yaml).toEqual(expectedYaml)
   })
 })
 
 interface TestContext {
-  store: Store<any>
+  store: Store<object>
   getters: StatesGetters & TransitionGetters
-  actions: StatesActions & UndoActions & TransitionsActions & SerializeActions & SoundsActions
+  actions: StatesActions & UndoActions & TransitionsActions & SerializeActions & SoundsActions & { $store: Store<object> }
 }
 
 function initTestContext (): TestContext {
-  const store = new Vuex.Store({
+  const store = new Vuex.Store<object>({
     modules: {
       info: infoModule({
         info: {
@@ -183,54 +163,14 @@ function initTestContext (): TestContext {
     store,
     getters,
     actions: {
-      addState (spec: StateSpec): Promise<StateSummary> {
-        return store.dispatch(ADD_STATE, spec)
-      },
-      removeState (stateId: string): void {
-        store.dispatch(REMOVE_STATE, stateId)
-      },
-      continueUpdateState (spec: StateSpec): void {
-        store.dispatch(CONTINUE_UPDATE_STATE, spec)
-      },
-      updateState (spec: StateSpec): Promise<StateSummary> {
-        return store.dispatch(UPDATE_STATE, spec)
-      },
-      focusState (id: string): void {
-        store.dispatch(FOCUS_STATE, id)
-      },
-      setInitialState (id: string): void {
-        store.dispatch(SET_INITIAL_STATE, id)
-      },
-      undo () {
-        store.dispatch(UNDO)
-      },
-      redo () {
-        store.dispatch(REDO)
-      },
-      addTransition (spec: TransitionSpec) {
-        return store.dispatch(ADD_TRANSITION, spec)
-      },
-      removeTransition (transitionId: string) {
-        store.dispatch(REMOVE_TRANSITION, transitionId)
-      },
-      serialize () {
-        return store.dispatch(SERIALIZE)
-      },
-      toYaml () {
-        return store.dispatch(TO_YAML)
-      },
-      addSound (spec: SoundSpec) {
-        return store.dispatch(ADD_SOUND, spec)
-      },
-      removeSound (id: string) {
-        store.dispatch(REMOVE_SOUND, id)
-      },
-      updateSound (updated: SoundSpec) {
-        store.dispatch(UPDATE_SOUND, updated)
-      },
-      serializeSounds (): Promise<PhonebookSubsetForSounds> {
-        return store.dispatch(SERIALIZE_SOUNDS)
-      }
+      $store: store,
+      ...mapActions({
+        ...statesActionMapping,
+        ...undoActionMapping,
+        ...transitionActionMapping,
+        ...soundsActionMapping,
+        ...serializeActionMapping
+      })
     }
   }
 }
